0fe3e6ef833a85d2b0e6b0c521a3d9b5d796bfac
alejandrogomez <alegomez1@me.com>
2019-09-18 13:04:51 -0400

    nav links properly redirect

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -1,27 +1,42 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Character = require('../models/character')
 
 //List all characters
-router.get('/character', (req, res, next)=>{
+router.get('/character', (req, res, next) => {
 
   Character.find()
-  .then((allCharacters)=>{
-    res.render('character/listOfCharacters', {characters: allCharacters})
-  })
-  .catch((err)=>{
-    next(err)
-  })
+    .then((allCharacters) => {
+      res.render('character/listOfCharacters', {
+        characters: allCharacters
+      })
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 //Create new character
-router.get('/character/create-new-character', (req, res, next)=>{
+router.get('/character/create-new-character', (req, res, next) => {
   res.render('character/newCharacter')
 })
 //Save creation
-router.post('/character/creation', (req, res, next)=>{
+router.post('/character/creation', (req, res, next) => {
 
+  let firstName   = req.body.firstName;
+  let lastName    = req.body.lastName;
+  let image       = req.body.image
+
+  Character.create({
+    firstName:  firstName,
+    lastName:   lastName,
+    image:      image
+  })
+  .then((result)=>{
+    res.redirect('/character')
+  })
 })
+
 
 
 
