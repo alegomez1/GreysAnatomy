d580fcd42b9cea9a374b56ebb48678d62d840982
alejandrogomez <alegomez1@me.com>
2019-09-18 12:29:21 -0400

    Adding more routes

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -4,8 +4,23 @@ const Character = require('../models/character')
 
 //List all characters
 router.get('/characters', (req, res, next)=>{
-  console.log('WORKING')
-  res.render('character/listOfCharacters')
+
+  Character.find()
+  .then((allCharacters)=>{
+    res.render('character/listOfCharacters', {characters: allCharacters})
+  })
+  .catch((err)=>{
+    next(err)
+  })
+})
+
+//Create new character
+router.get('/character/create-new-character', (req, res, next)=>{
+  res.render('character/newCharacter')
+})
+//Save creation
+router.post('/character/creation', (req, res, next)=>{
+  
 })
 
 
