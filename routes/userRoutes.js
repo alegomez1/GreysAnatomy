76ad72d984dea2d92e23b1016c19385882f76a16
alejandrogomez <alegomez1@me.com>
2019-09-19 10:42:27 -0400

    Added user model

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Character = require('../models/character')
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
+//Sign up page
 router.get('/account/signup', (req,res,next)=>{
   res.render('account/signup')
-  
+})
+//Save sign up creation
+router.post('/account/signup', (req, res, next)=>{
+
 })
 
 
